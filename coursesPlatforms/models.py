2ddf60d9c5810f25b0e7a4ee540effac52263e4d
stackphantom007 <soroushrahmati01@gmail.com>
2025-07-13 17:23:39 +0330

    feat(models): add Course model with access and publish status

--- coursesPlatforms/models.py
+++ coursesPlatforms/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+# What we are building
 
-# Create your models here.
+# Courses:
+
+# Title
+# Description
+# Thumbnail/Image
+# Access:
+# Anyone
+# Email required
+# Purchase required
+# User required (n/a)
+# Status:
+# Published
+# Coming Soon
+# Draft
+# Lessons
+# Title
+# Description
+# Video
+# Status: Published, Coming Soon, Draft
+# Email verification for short-lived access
+
+# Views:
+# Collect user email
+# Verify user email
+# Activate session
+# Models:
+# Email
+# EmailVerificationToken
+
+class AccessRequirement(models.TextChoices):    #ین یه کلاس کمکیه که با TextChoices ساخته شده و در واقع یه enum (لیست انتخابی) برای وضعیت انتشار دوره است.
+    ANY = "any" , "Anyone"
+    EMAIL_REQUIRED = "email_required" , "Email_required "
+
+
+
+class PublishStatus(models.TextChoices):    #ین یه کلاس کمکیه که با TextChoices ساخته شده و در واقع یه enum (لیست انتخابی) برای وضعیت انتشار دوره است.
+    PUBLISHED = "publish" , "Published"
+    COMING_SOON = "soon" , "Coming Soon"
+    DRAFT = "draft" , "Draft"
+
+class Course (models.Model):  #معرفی دوره آموزشی
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    access = status = models.CharField(max_length=10 , choices=AccessRequirement.choices,default= AccessRequirement.DRAFT)
+    #image
+    status = models.CharField(max_length=10 , choices=PublishStatus.choices,default= PublishStatus.DRAFT)
+    
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
